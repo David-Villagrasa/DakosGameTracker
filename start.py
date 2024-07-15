2a4b2e7c58f2d0eb75ace174ac566fe665d436de
David Villagrasa <107490517+David-Villagrasa@users.noreply.github.com>
2024-07-15 14:00:53 +0200

    Structure of files changed
    Files sorted in a better way

---
+++ start.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5 import QtWidgets
+from main import MainWindow
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec_())
